a10c4cf7df3d6afb117cd8438793ae0bb604d104
Abdul Rahman Haqiqi <abdul.rahman.haqiqi@example.com>
2026-09-21 11:15:27 +0700

    Add missing Katalon meta files (.tc and .ts)

---
+++ Test Suites/TS_HackerRank_Assessment.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_HackerRank_Assessment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e4c12345-6789-4abc-9def-000000000003</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5d12345-6789-4abc-9def-000000000004</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_REST_Member_Lifecycle</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5d12345-6789-4abc-9def-000000000005</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02_Kafka_Consumer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
